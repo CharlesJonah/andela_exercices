0e926c0d658c74229f4e9a712229eec1a9620d57
CharlesJonah <mutua.charles48@gmail.com>
2016-12-01 02:59:42 +0300

    Update prime_correct_code.py

--- prime_correct_code.py
+++ prime_correct_code.py
@@ -1,14 +1,28 @@
 
+#Code written in Python 3
 def prime_num(number):
     output = []
-    for num in range(2, number+1):
-        prime = True
-        for i in range(2, num):
-            if(num % i == 0):
-                prime = False
-        if prime:
-            output.append(num)
+    #Loop to get number n to use for testing
+    if isinstance(number,int):
+        if number > 1:
+            for num in range(2, number+1):
+                prime = True
+                for i in range(2, num):
 
-    return output
+                    #If the modulus of num and i is zero the disqulaify the number from being a prime
+                    if(num % i == 0):
+                        prime = False
+                if prime:
+                    #append the results to a list
+                    output.append(num)
 
-print(prime_num(100))
+            return output
+        else:
+            print("Enter number greater than 1.")
+
+
+    else:
+        print("Only numbers allowed.")
+
+print(prime_num('30'))
+
